d6ac4e0aaf59f389c312f2485b52235f188c595e
Taibu Haroub <haroubtaibu@gmail.com>
2021-07-05 00:37:56 +0300

    Test the auth middleware

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -1,7 +1,7 @@
 const expect = require("chai").expect;
 const authMiddleware = require("../middleware/is-auth");
 
-//will test the behaviour when it returns null
+//will test the behaviour when it returns null.
 
 it("should throw an error if no header is present", function() {
     const req = {
