92c2f634059d61a19450daee493c38802b872745
Taibu Haroub <haroubtaibu@gmail.com>
2021-07-05 13:24:07 +0300

    What not to test

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -3,42 +3,65 @@ const authMiddleware = require("../middleware/is-auth");
 
 //will test the behaviour when it returns null.
 
-it("should throw an error if no header is present", function() {
-    const req = {
-        get: function(headerName) {
-            return null;
-        }
-    }
-    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw("Not authenticated.");
-})
+it("should throw an error if no header is present", function () {
+  const req = {
+    get: function (headerName) {
+      return null;
+    },
+  };
+  expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
+    "Not authenticated."
+  );
+});
 
-it("should throw an error if the authorization is only one string", function() {
-    const req = {
-        get: function(headerName) {
-            return "xghs";
-        }
-    }
-    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
-})
-
+it("should throw an error if the authorization is only one string", function () {
+  const req = {
+    get: function (headerName) {
+      return "xghs";
+    },
+  };
+  expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+});
 
 //Grouping tests
-describe("Auth middleware", function() {
-    it("should throw an error if no header is present", function() {
-        const req = {
-            get: function(headerName) {
-                return null;
-            }
-        }
-        expect(authMiddleware.bind(this, req, {}, () => {})).to.throw("Not authenticated.");
-    })
-    
-    it("should throw an error if the authorization is only one string", function() {
-        const req = {
-            get: function(headerName) {
-                return "xghs";
-            }
-        }
-        expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
-    })
-})
+describe("Auth middleware", function () {
+  it("should throw an error if no header is present", function () {
+    const req = {
+      get: function (headerName) {
+        return null;
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
+      "Not authenticated."
+    );
+  });
+
+  it("should throw an error if the authorization is only one string", function () {
+    const req = {
+      get: function (headerName) {
+        return "xghs";
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+
+  it("should throw an error if the token cannot be verified", function () {
+    const req = {
+      get: function (headerName) {
+        return "Bearer xyz";
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+
+  it("should yeild a userId after decoding the token", function () {
+    const req = {
+      get: function (headerName) {
+        return "Bearer xyziiwoowddawwwsas";
+      },
+    };
+    // will manually call the authMiddleware function
+    authMiddleware(req, {}, () => {});
+    expect(req).to.have.property("userId");
+  });
+});
