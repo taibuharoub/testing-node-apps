30d1de4da11d08ca7ab5fdaba7f3b6ddab74e938
Taibu Haroub <haroubtaibu@gmail.com>
2021-07-05 13:00:56 +0300

    Organize/Group mutiple test

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -11,3 +11,34 @@ it("should throw an error if no header is present", function() {
     }
     expect(authMiddleware.bind(this, req, {}, () => {})).to.throw("Not authenticated.");
 })
+
+it("should throw an error if the authorization is only one string", function() {
+    const req = {
+        get: function(headerName) {
+            return "xghs";
+        }
+    }
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+})
+
+
+//Grouping tests
+describe("Auth middleware", function() {
+    it("should throw an error if no header is present", function() {
+        const req = {
+            get: function(headerName) {
+                return null;
+            }
+        }
+        expect(authMiddleware.bind(this, req, {}, () => {})).to.throw("Not authenticated.");
+    })
+    
+    it("should throw an error if the authorization is only one string", function() {
+        const req = {
+            get: function(headerName) {
+                return "xghs";
+            }
+        }
+        expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+    })
+})
