f607c869be5464d02d453cdd7b005ae9a578f3c3
Taibu Haroub <haroubtaibu@gmail.com>
2021-07-05 15:25:56 +0300

    Test Async code

--- controllers/auth.js
+++ controllers/auth.js
@@ -60,12 +60,15 @@ exports.login = async (req, res, next) => {
       { expiresIn: '1h' }
     );
     res.status(200).json({ token: token, userId: loadedUser._id.toString() });
+    return;
   } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500;
     }
     next(err);
+    return err;
   }
+  // return; //will implictly return the promise 
 };
 
 exports.getUserStatus = async (req, res, next) => {
--- test/auth-controller.js
+++ test/auth-controller.js
@@ -4,9 +4,26 @@ const sinon = require("sinon");
 const User = require("../models/user");
 const AuthController = require("../controllers/auth");
 
-describe("Auth Controller - Login", function() {
-    it("should throw an error with code 500 if accessing the database fails", function() {
-        sinon.stub(User, "findOne");
-        User.findOne.throws();
-    })
-})
+describe("Auth Controller - Login", function () {
+  it("should throw an error with code 500 if accessing the database fails", function (done) {
+    sinon.stub(User, "findOne");
+    User.findOne.throws();
+
+    const req = {
+      body: {
+        email: "ty@ty.com",
+        password: "tester",
+      },
+    };
+
+    AuthController.login(req, {}, () => {}).then((result) => {
+    //   console.log(result);
+    expect(result).to.be.an("error");
+    expect(result).to.have.property("statusCode", 500);
+    // expect(result).to.have.property("statusCode", 401); //to tigger error
+    done() //will signal to mocha to wait for this code to finish to execute 
+    }).catch(done)
+
+    User.findOne.restore();
+  });
+});
